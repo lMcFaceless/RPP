2e2ae9e50d7439fc092c13cb14f916195e92ae5a
lMcFaceless <jxctrhtnyjlab@gmail.com>
2023-03-04 12:29:32 +0400

    Случайно сделал 22 вариант вместо 23, исправил.

--- rppLab1/lab1.py
+++ rppLab1/lab1.py
@@ -1,29 +1,27 @@
 import random
 
 
-def quick_sort(list_length):
-    if len(list_length) > 1:
-        random_number = list_length[random.randint(0, len(list_length) - 1)]  # Выбор случайного значения
-        number_smaller = [u for u in list_length if u < random_number]  # Создание списка элементов меньше случайного
-        number_equals = [u for u in list_length if u == random_number]  # Создание списка элементов равных случайному
-        number_bigger = [u for u in list_length if u > random_number]  # Создание списка элементов больше случайного
-        list_length = quick_sort(number_smaller) + number_equals + quick_sort(number_bigger)  # "Пересборка" массива
-    return list_length
-
-
-def input_list():
-    numberList = []
-    try:
-        numberList = [int(el) for el in input().split()]    # Ввод с клавиатуры
-    except:
-        print('Неправильный формат данных')
-
+def bubble_sort(numberList):
+    for i in range(len(numberList) - 1):
+        for j in range(len(numberList) - i - 1):
+            if numberList[j] > numberList[j + 1]:
+                numberList[j], numberList[j + 1] = numberList[j + 1], numberList[j]  # Элементы меняются местами
     return numberList
 
 
-numberList = input_list()   # Вызов метода для ввода массива
-numberList = quick_sort(numberList)  # Вызов функции
-numberListRandom = [random.randint(-10, 10) for _ in range(10)]
-numberListRandom = quick_sort(numberListRandom)
-print(numberList)  # Вывод отсортированного массива
-print(numberListRandom)  # Вывод отсортированного случайного массива
+def input_list():
+    inputList = []
+    try:
+        inputList = [int(el) for el in input().split()]  # Ввод с клавиатуры
+    except:
+        print('Неправильный формат данных')
+
+    return inputList
+
+
+numberList = input_list()  # Вызов метода для ввода массива
+numberList = bubble_sort(numberList)  # Вызов метода для сортировки массива
+numberListRandom = [random.randint(-10, 10) for _ in range(10)]  # Заполнение массива случайными числами
+numberListRandom = bubble_sort(numberListRandom)  # Вызов метода для сортировки случайного массива
+print('Отсортированный массив: ', numberList)  # Вывод отсортированного массива
+print('Отсортированный случайный массив: ', numberListRandom)  # Вывод отсортированного случайного массива
