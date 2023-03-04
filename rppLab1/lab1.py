57a2944dee81063f597b697f37642388d63f70c9
lMcFaceless <jxctrhtnyjlab@gmail.com>
2023-03-04 12:56:43 +0400

    Случайно сделал 22 вариант вместо 23, исправил.

--- rppLab1/lab1.py
+++ rppLab1/lab1.py
@@ -1,12 +1,17 @@
 import random
 
 
-def bubble_sort(numberList):
-    for i in range(len(numberList) - 1):
-        for j in range(len(numberList) - i - 1):
-            if numberList[j] > numberList[j + 1]:
-                numberList[j], numberList[j + 1] = numberList[j + 1], numberList[j]  # Элементы меняются местами
+def bubble_sort(sort_list):
+    for i in range(len(sort_list) - 1):
+        for j in range(len(sort_list) - i - 1):
+            if sort_list[j] > sort_list[j + 1]:
+                sort_list[j], sort_list[j + 1] = sort_list[j + 1], sort_list[j]  # Элементы меняются местами
     return numberList
+
+
+def st_sort(st_sort_list):  # Сортировка массива с использованием стандартной функции
+    list.sort(st_sort_list)
+    return st_sort_list
 
 
 def input_list():
@@ -22,6 +27,6 @@ def input_list():
 numberList = input_list()  # Вызов метода для ввода массива
 numberList = bubble_sort(numberList)  # Вызов метода для сортировки массива
 numberListRandom = [random.randint(-10, 10) for _ in range(10)]  # Заполнение массива случайными числами
-numberListRandom = bubble_sort(numberListRandom)  # Вызов метода для сортировки случайного массива
+numberListRandom = st_sort(numberListRandom)  # Вызов метода для сортировки случайного массива
 print('Отсортированный массив: ', numberList)  # Вывод отсортированного массива
 print('Отсортированный случайный массив: ', numberListRandom)  # Вывод отсортированного случайного массива
